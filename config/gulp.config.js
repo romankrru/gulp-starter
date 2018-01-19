9fab498a95d2651265495f2d921aeb7bd7764ba0
Roman <rm07ru@gmail.com>
2018-01-19 19:14:34 +0300

    Fix typos

--- config/gulp.config.js
+++ config/gulp.config.js
@@ -3,24 +3,24 @@ const config = {
     src: './src/pages/*.html',
     build: './build',
     watch: './src/pages/**/*.html',
-    tmp: './.tmp'
+    tmp: './.tmp',
   },
   style: {
     src: './src/common/*.styl',
     build: './build/css',
     tmp: './.tmp/css',
-    watch: ['./src/blocks/**/*.styl', './src/common/*.styl']
+    watch: ['./src/blocks/**/*.styl', './src/common/*.styl'],
   },
   image: {
     src: ['./src/img/**/*.jpg', './src/img/**/*.png', './src/img/**/*.svg'],
     tmp: './.tmp/img',
-    build: './build/img'
+    build: './build/img',
   },
   fonts: {
     src: ['./src/fonts/**/*'],
     tmp: './.tmp/fonts',
-    build: './build/fonts'
-  }
+    build: './build/fonts',
+  },
 };
 
 module.exports = config;
